4822e68d6fe3ff2e89df6001862597857fcc4a9e
David Wu <david204code@gmail.com>
2020-02-26 14:20:01 +0000

    Started building router with react-dom-router but incomplete

---
+++ app/javascript/components/Router.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter, Router, Switch } from 'react-router-dom';
+
+const Router = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path ="/" component ={}/>
+    </Switch>
+  </BrowserRouter>
+
+);
+
+export default Router;
+
+
+
+
+
+
+
+
+
+
+
+
