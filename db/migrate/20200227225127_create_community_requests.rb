e20a1d6fe1fa27e69b5a41baf28f1aafe7244b2f
David Wu <david204code@gmail.com>
2020-02-27 23:03:13 +0000

    created the community_request model and table

---
+++ db/migrate/20200227225127_create_community_requests.rb
@@ -0,0 +1,15 @@
+class CreateCommunityRequests < ActiveRecord::Migration[6.0]
+  def change
+    create_table :community_requests do |t|
+      t.string "title"
+      t.text "description"
+      t.string "request_type"
+      t.string "location_lat"
+      t.string "location_long"
+      t.string "status"
+      t.boolean "fulfilled", default: false 
+
+      t.timestamps
+    end
+  end
+end
