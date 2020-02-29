f5d7493387ed99ed6ab5aee97fce5a5e06b31acf
David Wu <david204code@gmail.com>
2020-02-29 21:55:47 +0000

    Added Marker in the ReactMapGL

--- app/javascript/components/map.js
+++ app/javascript/components/map.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react';
 import {render} from 'react-dom';
-import MapGL from 'react-map-gl';
+// import MapGL from 'react-map-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
+import ReactMapGL, {Marker} from 'react-map-gl';
 
 const TOKEN = 'pk.eyJ1IjoiZGF2aWQyMDRjb2RlMSIsImEiOiJjazc2YjdobGUwOTI0M2VvamwwZXpvZGR1In0.FSpShMuhbroEHA9-0iG4sg';
 
@@ -10,9 +11,9 @@ class Map extends Component {
     super(props);
     this.state = {
       viewport: {
-        latitude: 51.5074,
-        longitude: 0.1278,
-        zoom: 10,
+        latitude: 51.508,
+        longitude: -0.140,
+        zoom: 14,
         bearing: 0,
         pitch: 0
       }
@@ -21,14 +22,24 @@ class Map extends Component {
 
   render() {
     return (
-      <MapGL
+      <ReactMapGL
         {...this.state.viewport}
         width="80vw"
         height="80vh"
-        mapStyle="mapbox://styles/mapbox/dark-v9"
+        mapStyle="mapbox://styles/mapbox/streets-v11"
         onViewportChange={viewport => this.setState({viewport})}
         mapboxApiAccessToken={TOKEN}
-      />
+        >
+          <Marker 
+          latitude={51.508} 
+          longitude={-0.140} 
+          offsetLeft={-20} 
+          offsetTop={-10}
+          >
+            <button>You are here</button>
+          </Marker>
+        </ReactMapGL>
+      
     );
   }
 }
