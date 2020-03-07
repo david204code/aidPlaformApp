c008271273b54f2785e76e018c60cca42d3284af
David Wu <david204code@gmail.com>
2020-03-07 18:54:38 +0000

    updated the key _aidplaformapp in session_store.rb

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 if Rails.env == "production"
-  Rails.application.config.session_store :cookie_store, key: "_authentication_app", 
+  Rails.application.config.session_store :cookie_store, key: "_aidplaformapp", 
   domain: "myheroku.com"
 else
-  Rails.application.config.session_store :cookie_store, key: "_authentication_app"
+  Rails.application.config.session_store :cookie_store, key: "_aidplaformapp"
 end
